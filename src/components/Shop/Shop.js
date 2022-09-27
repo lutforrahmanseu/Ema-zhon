6816732e0713c3d48b5234e6471a48aa1d0c29f4
Md.Lutfor Rahman <61869128+lutforrahmanseu@users.noreply.github.com>
2022-09-27 20:49:36 +0600

    secound commit

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
+import { addToDb, getStoredCart } from "../../utilities/fakedb";
+import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
 const Shop = () => {
@@ -11,11 +13,36 @@ const Shop = () => {
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+  useEffect(() => {
+    const storedCart = getStoredCart();
+    const saveCart = [];
+    for (const id in storedCart) {
+      const addProduct = products.find((product) => product.id === id);
+      if (addProduct) {
+        const quantity = storedCart[id];
+        addProduct.quantity = quantity;
+        saveCart.push(addProduct);
+      }
+    }
+    setCart(saveCart);
+  }, [products]);
 
-  const handlerAddToCart = (product) => {
-    const newCart = [...cart, product];
+  const handlerAddToCart = (selectedProduct) => {
+    let newCart = [];
+    const exists = cart.find((product) => product.id === selectedProduct);
+    if (!exists) {
+      selectedProduct.quantity = 1;
+      newCart = [...cart, selectedProduct];
+    } else {
+      const rest = cart.filter((product) => product.id !== selectedProduct.id);
+      exists.quantity = exists.quantity + 1;
+      newCart = [...rest, exists];
+    }
+
     setCart(newCart);
+    addToDb(selectedProduct.id);
   };
+
   return (
     <div className="shop-container">
       <div className="products-container">
@@ -28,8 +55,7 @@ const Shop = () => {
         ))}
       </div>
       <div className="cart-container">
-        <h4>Order Summary</h4>
-        <p>Selected Items:{cart.length}</p>
+        <Cart cart={cart}></Cart>
       </div>
     </div>
   );
